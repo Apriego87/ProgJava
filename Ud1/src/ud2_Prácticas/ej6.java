4576e21218ebf3d88e6376653c16589f0c8de111
Apriego87 <antoniopriego22@gmail.com>
2022-10-03 10:44:21 +0200

    ej6 format

--- Ud1/src/ud2_Prácticas/ej6.java
+++ Ud1/src/ud2_Prácticas/ej6.java
@@ -8,16 +8,17 @@ public class ej6 {
         System.out.println("Introduce el consumo de agua: ");
         float consum = scan.nextFloat();
         if (consum < 100) {
-            System.out.printf("Deberás pagar: %.2f euros." , consum * 0.15);
+            System.out.printf("Deberás pagar: %.2f euros.", consum * 0.15);
         }
         if (consum >= 100 && consum < 500) {
-            System.out.printf("Deberás pagar: %.2f euros." , ((100 * 0.15)+((consum-100)*0.20)));
+            System.out.printf("Deberás pagar: %.2f euros.", ((100 * 0.15) + ((consum - 100) * 0.20)));
         }
         if (consum >= 500 && consum < 1000) {
-            System.out.printf("Deberás pagar: %.2f euros." , ((100 * 0.15)+(400*0.20)+((consum-500)*0.35)));
+            System.out.printf("Deberás pagar: %.2f euros.", ((100 * 0.15) + (400 * 0.20) + ((consum - 500) * 0.35)));
         }
         if (consum >= 1000) {
-            System.out.printf("Deberás pagar: %.2f euros." , ((100 * 0.15)+(400*0.20)+(900*0.35)+((consum-1000)*0.8)));
+            System.out.printf("Deberás pagar: %.2f euros.",
+                    ((100 * 0.15) + (400 * 0.20) + (900 * 0.35) + ((consum - 1000) * 0.8)));
         }
     }
 }
