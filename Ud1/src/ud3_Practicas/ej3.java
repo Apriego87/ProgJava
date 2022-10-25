bfa218e3272928b12b00a328a68f62e44b134c82
Apriego87 <antoniopriego22@gmail.com>
2022-10-25 14:18:40 +0200

    hasta el 4 (con errores)

---
+++ Ud1/src/ud3_Practicas/ej3.java
@@ -0,0 +1,14 @@
+package ud3_Practicas;
+
+import java.util.Scanner;
+
+public class ej3 {
+    public static double dolarAeuro(double cantid, double cotiz) {
+        return cantid * cotiz;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Introduce una cantidad en dólares y la cotización actual del dólar (separados por espacios): ");
+        
+    }
+}
