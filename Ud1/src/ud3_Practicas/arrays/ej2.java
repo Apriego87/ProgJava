65009189e2fd207e89b4ecc8de5cdb38c776a1b4
Apriego87 <antoniopriego22@gmail.com>
2022-11-02 10:06:36 +0100

    hasta el 5 (formatted)

--- Ud1/src/ud3_Practicas/arrays/ej2.java
+++ Ud1/src/ud3_Practicas/arrays/ej2.java
@@ -6,8 +6,8 @@ public class ej2 {
     public static void main(String[] args) {
         int[] arr = new int[20];
 
-        for(int i = 0; i<20;i++){
-            arr[i] = (int)(Math.random() * 100);
+        for (int i = 0; i < 20; i++) {
+            arr[i] = (int) (Math.random() * 100);
         }
 
         System.out.println(Arrays.toString(arr));
--- Ud1/src/ud3_Practicas/arrays/ej3.java
+++ Ud1/src/ud3_Practicas/arrays/ej3.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class ej3 {
     public static void main(String[] args) {
         int[] arr = new int[12];
-        arr[0]=39;
-        arr[1]=-2;
-        arr[4]=0;
-        arr[6]=14;
-        arr[8]=5;
-        arr[9]=120;
+        arr[0] = 39;
+        arr[1] = -2;
+        arr[4] = 0;
+        arr[6] = 14;
+        arr[8] = 5;
+        arr[9] = 120;
 
         System.out.print(Arrays.toString(arr));
 
