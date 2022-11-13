3f69334bb7b3cf5eb103aefced3b797729fca873
Apriego87 <antoniopriego22@gmail.com>
2022-11-13 18:10:53 +0100

    hasta el número prohibido

--- Ud1/src/ud3_Practicas/repaso/ej1.java
+++ Ud1/src/ud3_Practicas/repaso/ej1.java
@@ -5,25 +5,52 @@ import java.util.Scanner;
 
 public class ej1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
         int[] nums = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        leer(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        int[] max = valorMax(nums);
+        int[] min = valorMin(nums);
+
+        System.out.printf("El valor máximo es %d, posición %d.\n", max[0], max[1]);
+        System.out.printf("El valor mínimo es %d, posición %d.", min[0], min[1]);
+
+        
+    }
+
+    public static void leer(int[] nums) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Introducción de elementos en el array.\n");
+        for (int i = 0; i < nums.length; i++) {
             System.out.print("Introduce un número: ");
-            introd(nums, scan.nextInt());
+            nums[i] = scan.nextInt();
         }
-
-        
+        scan.close();
     }
 
-    public static void introd(int[] nums, int num) {
-        for (int i = 0; i < 10; i++) {
-            nums[i] = num;
+    public static int[] valorMax(int[] nums) {
+        int max = nums[0];
+        int pos = 0;
+        for (int i = 1; i < 10; i++){
+            if (nums[i] > max){
+                max = nums[i];
+                pos = i;
+            }
         }
+        return new int[]{max,pos};
     }
 
-    public static void valorMax(int[] nums) {
-        
+    public static int[] valorMin(int[] nums) {
+        int min = nums[0];
+        int pos = 0;
+        for (int i = 1; i < 10; i++){
+            if (nums[i] < min){
+                min = nums[i];
+                pos = i;
+            }
+        }
+        return new int[]{min,pos};
     }
 }
