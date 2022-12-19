c14731dd9d60ccb9fca37d96a4ac74720bad887f
Apriego87 <antoniopriego22@gmail.com>
2022-12-19 19:54:15 +0100

    va a ordenar quien yo te diga

---
+++ Ud1/src/ud3_2_Practicas/colecciones3/TituloComparator.java
@@ -0,0 +1,11 @@
+package ud3_2_Practicas.colecciones3;
+
+import java.util.Comparator;
+
+public class TituloComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
